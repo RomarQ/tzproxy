d64e6e7dd0520604dee33c01c6e21d43d8de3ae1
RoMarQ <quelhas95@gmail.com>
2019-05-30 11:37:53 +0100

    Added support for lets-encrypt, this enables HTTPS

--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt-get -y install nginx
 sudo update-rc.d nginx enable
 sudo rm -f /etc/nginx/nginx.conf
 wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/RomarQ/tzproxy/master/nginx.conf
 sudo nginx -s reload
-exit
+
+while test $# -gt 0; do
+    case "$1" in
+        --ssl)
+            echo "|> Let's Encrypt's"
+            sudo apt-get install software-properties-common
+            sudo add-apt-repository universe
+            sudo add-apt-repository ppa:certbot/certbot
+            sudo apt-get update
+            sudo apt-get install certbot python-certbot-nginx
+            sudo certbot --nginx
+            sudo certbot renew --dry-run
+            exit;
+            ;;
+        *)
+            echo "$1 is not a recognized flag!"
+            exit;
+            ;;
+    esac
+done
+
